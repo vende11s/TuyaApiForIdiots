46c0626bb978153b3b6308d680e732c43722cc4c
vende <vende@vende>
2022-12-11 01:54:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# what is TuyaApiForIdiots?
+* python code for steering tuya light bulbs
+* it's wrapper of wrapper of wrapper
+* every idiot can use it, it couldn't be more simple
+
+# usage
+* connect your bulb to Tuya app
+* go to Tuya iot, create project, add device, you'll find at the internet how to do it
+* find ip of your bulb, you can use nmap: `sudo nmap -sn 192.168.<>.<>/24` or some android app
+* (optional but recommended) go to your router->dhcp settings and set constant ip for your bulb
+* install (tuya cli)[https://github.com/TuyaAPI/cli] `npm i @tuyapi/cli -g`
+* run `tuya-cli wizard`, use keys from Tuya iot, grab device key
+* put id, key and ip to `tuya.py` (and maybe protocol version, when i'm writing it the actual version is 3.3)
+* run `tuya.py -h` to learn possible options
+* And that's it! You can steer tuya light bulb from pc in the easiest way possible!
---
+++ tuya.py
@@ -0,0 +1,56 @@
+import os
+import argparse
+
+IP = ""
+ID = ""
+KEY = ""
+PROTOCOL_VERSION = "3.3"
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--on', help="turns light on", required=False, action='store_true')
+parser.add_argument('--off', help="turns light off", required=False, action='store_true')
+parser.add_argument('--bright','-b', help="sets bright of the light, <1-100>", required=False, type=int)
+parser.add_argument('-w', '--warm', help="sets warmness level for white color, <0-1000>", required=False, type=int)
+parser.add_argument('-c', '--color', help="sets color to: white, red, blue, green, pink", required=False, type=str)
+
+args = parser.parse_args()
+
+if args.on:
+    os.system("tuya-cli set --ip %s --id %s --key %s --protocol-version %s --dps 20 --set true" % (IP, ID, KEY,PROTOCOL_VERSION))
+
+if args.off:
+    os.system("tuya-cli set --ip %s --id %s --key %s --protocol-version %s --dps 20 --set false" % (IP, ID, KEY,PROTOCOL_VERSION))
+    
+if args.bright != None:
+    if args.bright < 1:
+        print("for bright you can use only values <1-100>")
+        quit()
+    os.system("tuya-cli set --ip %s --id %s --key %s --protocol-version %s --dps 22 --set %d" % (IP, ID, KEY,PROTOCOL_VERSION, args.bright*10))
+
+if args.warm != None:
+    os.system("tuya-cli set --ip %s --id %s --key %s --protocol-version %s --dps 23 --set %d" % (IP, ID, KEY,PROTOCOL_VERSION, args.warm))
+
+def setColor(color):
+    if color == "white":
+        os.system("tuya-cli set --ip %s --id %s --key %s --protocol-version %s --dps 21 --set white" % (IP, ID, KEY,PROTOCOL_VERSION))
+        return
+    #those hex values are hsv color but hue goes normally, and saturation/value goes from 10-1000
+    colors = {
+        "red" : "016803e803e8",
+        "blue" : "00F003e803e8",
+        "green" :  "007303e803e8",
+        "pink" :  "012c03e803e8"
+    }
+
+    if color not in colors:
+        print("not supported color")
+        return
+        
+    os.system("tuya-cli set --ip %s --id %s --key %s --protocol-version %s --dps 21 --set colour" % (IP, ID, KEY,PROTOCOL_VERSION))
+    os.system("tuya-cli set --ip %s --id %s --key %s --protocol-version %s --dps 24 --set %s" % (IP, ID, KEY,PROTOCOL_VERSION, colors.get(color)))
+
+if args.color != None:
+    setColor(args.color)
+
+
